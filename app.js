6cef26d0095384ba205ea7056fc4c6f668cdeed1
Rafael Martins Herculano <rafael.herculano@bbtecno.com.br>
2019-02-26 10:38:56 -0300

    Métodos

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Vue training'
+        title: 'Vue training',
+        name: 'Rafael'
+    }, 
+    methods:{
+        greet(time){
+            
+            return `Olá testando vue, este é o titulo da página: ${this.title}, ainda é ${time}, meu nome é ${this.name}`;
+        }
     }
 })
