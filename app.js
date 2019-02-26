f6c72e8d98f8aa5ea4be6e215fbd473a0c2d7270
Rafael Martins Herculano <rafael.herculano@bbtecno.com.br>
2019-02-26 11:58:43 -0300

    Event modifiers

--- app.js
+++ app.js
@@ -3,28 +3,11 @@ new Vue({
     data: {
         title: 'Vue training',
         name: 'Rafael',
-        url: "https://vuejs.org/v2/guide/installation.html",
-        classes: ['one', 'two'],
-        wage: 10, 
-        coords :{
-            x: 0,
-            y: 0
-        }
+        
     }, 
     methods:{
-        greet(time){
-            
-            return `Olá testando vue, este é o titulo da página: ${this.title}, ainda é ${time}, meu nome é ${this.name}`;
-        }, 
-        changeWage(amount){
-            this.wage += amount ;
-        },
-        logEvent(e){
-            console.log(e);
-        }, 
-        logCoords(e){
-            this.coords.x = e.offsetX ;
-            this.coords.y = e.offsetY ;
+        logMessage(){
+            console.log('log Message')
         }
     }
 })
