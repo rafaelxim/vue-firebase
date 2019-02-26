4d281997d13bede4973e9c8dc2ee7c78fd013b68
Rafael Martins Herculano <rafael.herculano@bbtecno.com.br>
2019-02-26 10:56:22 -0300

    Eventos

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ new Vue({
         title: 'Vue training',
         name: 'Rafael',
         url: "https://vuejs.org/v2/guide/installation.html",
-        classes: ['one', 'two']
+        classes: ['one', 'two'],
+        wage: 10
     }, 
     methods:{
         greet(time){
             
             return `Olá testando vue, este é o titulo da página: ${this.title}, ainda é ${time}, meu nome é ${this.name}`;
+        }, 
+        changeWage(amount){
+            this.wage += amount ;
         }
     }
 })
